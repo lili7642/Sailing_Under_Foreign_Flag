5cc256c192969c720d99c7e907665d95a486d39f
lili7642 <lili7642@student.uu.se>
2023-02-28 19:06:45 +0100

    Added comments

--- Database/script_funcs.js
+++ Database/script_funcs.js
@@ -12,6 +12,7 @@ function load_user_box(user_info){
 
 
 function show_info_popup(beverage){
+    // showing the info popup and setting the text
     $('#info-popup').show().html(
         '<form>' +
                 '<label>'+beverage.namn+'</label>' +
@@ -20,45 +21,55 @@ function show_info_popup(beverage){
                 '<p>Alkoholhalt: '+ beverage.alkoholhalt +'</p>' +
                 '<p>Pris: '+ beverage.prisinklmoms +' SEK</p>' +
         '</form>')
-        .attr("onclick", "style='display: none;'");
+        .attr("onclick", "style='display: none;'"); // on click the box disappears
 }
 
 
 function add_to_order(beverage) {
+    // relevant divs
     let orderDiv = $('#order-item-wrapper');
     let orderItem = $('<div class="order-item"></div>');
     let itemName = $('<span class="item-name"></span>').text(beverage.namn);
     let itemPrice = $('<span class="item-price"></span>').text(beverage.prisinklmoms + ' SEK');
 
-
+    // hide this message since order no longer empty
+    $('#empty-order-message').hide();
 
     // row below is CHAT GPT magic
     let existingItem = orderDiv.find('.item-name:contains("'+beverage.namn+'")').first().parent('.order-item');
 
+    // IF BEVERAGE ALREADY IN ORDER, CHANGE THE DIV INSTEAD OF ADDING
     if (beverage.namn in order){
         order[beverage.namn] += 1;
         existingItem.find('.item-name').text(beverage.namn + " x" + order[beverage.namn]);
         existingItem.find('.item-price').text(beverage.prisinklmoms + " SEK");
 
+    // ADD ORDER ITEM DIV
     }else{
         order[beverage.namn] = 1;
         orderItem.append(itemName);
         orderItem.append(itemPrice);
         orderDiv.append(orderItem);
     }
+    // UPDATE ORDER TOTAL
     orderTotal += parseFloat(beverage.prisinklmoms);
     $('#order-total').text('Total: ' + orderTotal.toFixed(2) + ' SEK');
 
 }
 
 function place_order(){
-
+    // RESET ORDER TOTAL AND ORDER LIST
     let temp_total = orderTotal;
     orderTotal = 0;
     order = {};
     $('#order-total').text('Total: ' + orderTotal.toFixed(2) + ' SEK');
 
-    $('#order-item-wrapper').empty();
+    // REMOVE ITEMS FROM BASKET
+    $('.order-item').remove();
+    // SHOW MESSAGE THAT BASKET IS EMPTY
+    $('#empty-order-message').show();
+
+
 
     if(current_user.credentials < 4){
         // Pays with credit
--- script.js
+++ script.js
@@ -2,15 +2,16 @@
 
 $(document).ready(function() {
 
-    //prefill form as default
+    //PREFILL THE FORM FOR TESTING
     $('#username').val('jorass');
     $('#password').val('b690bc2447d40ea8a6f78345eb979a28');
+
     $('#login_form').submit(function(e) {
         e.preventDefault();
         let username = $('#username').val();
         let password = $('#password').val();
 
-        //check if login valid
+        // CHECK LOGIN BY LOOPING THROUGH ALL STORED USERNAMES AND PASSWORDS
         let found = false;
         let firstname, lastname;
         for (let user of users) {
@@ -22,8 +23,12 @@ $(document).ready(function() {
         }
 
         if (found){
-            $('#login-message').text("Login successful!").removeClass('error').addClass('success').show().fadeOut(3000);
-            $('#login-message').click(function(){$(this).hide()});
+            // SHOW LOGIN MESSAGE AND HIDE THE LOGIN WINDOW
+            // LOGIN MESSAGE WILL FADE OUT OR DISAPPEAR IF CLICKED
+            $('#login-message').text("Login successful!")
+                .removeClass('error').addClass('success')  // change class of message
+                .show().fadeOut(5000)                            // fadeout after 5s
+                .click(function(){$(this).hide()});              //click to hide
             $('#login_form').hide();
             $('#menu').show();
 
@@ -55,23 +60,25 @@ $(document).ready(function() {
         $('#menu-item-wrapper').css("height","100%");
     });
 
-
+    // BUTTON FOR DEPOSITING MONEY
     $('#deposit-button').click(function (){
+        // SHOW THE POPUP FORM
         $('#popup').show();
     });
 
+    // READ MONEY TO DEPOSIT
     $('#popup form').submit(function (e){
         e.preventDefault();
         let deposit_amount = $('#number-input').val();
-        // CHANGE BALANCE AND UPDATE DIV
-
-        // fake balance, resets on reload
+        // fake balance, resets on reload !
+        // UPDATE BALANCE AND DIV
         current_balance = (Number(current_balance) + Number(deposit_amount)).toString();
         $('#credit').html("<b>Balance: </b>" + current_balance + " SEK");
         $('#popup').hide();
     });
 
     $('#popup').click(function(e) {
+        // IF YOU CLICK OUTSIDE THE POPUP IT WILL CLOSE
         if ($(e.target).attr('id') === 'popup') {
             $('#popup').hide();
         }
@@ -81,32 +88,29 @@ $(document).ready(function() {
     // Load the menu
     let menuDiv = $('#menu-item-wrapper');
     for (let beverage of beers) {
+        // LOAD THE DIVS
         let menuItem = $('<div class="menu-item" draggable="true"></div>');
         let itemName = $('<span class="item-name"></span>').text(beverage.namn);
         let infoButton = $('<span class="info-button"></span>').html(' &#9432');
         let itemButton = $('<button class="item-button"></button>').text(beverage.prisinklmoms + ' SEK');
-
         let someWrapper = $('<div></div>');
+
+        // STACK THEM
         someWrapper.append(itemName);
         someWrapper.append(infoButton);
-
-
         menuItem.append(someWrapper);
         menuItem.append(itemButton);
         menuDiv.append(menuItem);
 
-        itemButton.click(function (){
-            add_to_order(beverage);
-        });
-
-        infoButton.click(function () {
-            show_info_popup(beverage);
-        });
+        // ADD CLICK FUNCTIONS TO ORDER BUTTON AND INFO BUTTON
+        itemButton.click(function (){add_to_order(beverage);});
+        infoButton.click(function () {show_info_popup(beverage);});
     }
 
-$('#order-button').click(function (){
-    place_order();
-});
+    // ADD CLICK FUNCTION TO ORDER BUTTON
+    $('#order-button').click(function (){
+        place_order();
+    });
 
 
 
